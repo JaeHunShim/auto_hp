d241f6c31b3505269f599218085fdb1a22ed2bfd
JaeHunShim <jaehuniya@gmail.com>
2018-10-01 21:08:56 +0900

    mybatis mapper set

--- auto_hp/src/main/java/com/auto_hp/Application.java
+++ auto_hp/src/main/java/com/auto_hp/Application.java
@@ -1,9 +1,11 @@
 package com.auto_hp;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(value={"com.auto_hp.mapper"})
 public class Application {
 
 	public static void main(String[] args) {
