51b70be36aca29d25818d7626044b35b0fc3ab0b
JaeHunShim <jaehuniya@gmail.com>
2018-10-03 17:45:25 +0900

    login get add

---
+++ auto_hp/src/main/java/com/auto_hp/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.auto_hp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+	
+	@GetMapping("/")
+	public String main() {
+		return "redirect:/login";
+	}
+	@GetMapping("/login")
+	public void login() {
+		
+	}
+}
