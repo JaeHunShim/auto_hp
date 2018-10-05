1c7379e03e4ec1c67fda80513e59c02d3df76909
JaeHunShim <jaehuniya@gmail.com>
2018-10-05 22:11:59 +0900

    user table create

---
+++ user.sql
@@ -0,0 +1,12 @@
+create table user(
+	user_email varchar(50) not null,
+    user_pw varchar(50) not null,
+    user_nick varchar(50) not null,
+    user_address varchar(50) not null,
+    user_address2 varchar(50) not null,
+    user_tel varchar(50) not null,
+    regdate timestamp not null default now(),
+    update_date timestamp not null default now(),
+    primary key(user_email)
+    
+);
